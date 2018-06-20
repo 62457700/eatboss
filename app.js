604ac46805f41771f2cca8b81f7206df85f31084
62457700 <40421077+62457700@users.noreply.github.com>
2018-06-21 02:13:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var bot = linebot({
 var con = mysql.createConnection({
     host: "us-cdbr-iron-east-04.cleardb.net",
     user: "b39aa56085284b",
-    password: "71b22bf6 ",
+    password: "71b22bf6",
     database: "heroku_f73985ce8de4d0b"
 });
 
